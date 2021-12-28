a00a6cac7ad8129f060e8f06b56d6a77d139c5fc
Shaik Bikarsha <87685116+shaikbikarsha@users.noreply.github.com>
2021-12-29 01:10:00 +0530

    Create ConfigPropertyReader.java

---
+++ src/main/java/controllers/ConfigPropertyReader.java
@@ -0,0 +1,67 @@
+package controllers;
+
+import ru.qatools.properties.Property;
+import ru.qatools.properties.Resource;
+
+
+@Resource.Classpath({"config.properties"})
+public class ConfigPropertyReader {
+
+    @Property(value = "browser")
+    private String browser;
+
+    @Property(value = "maxPageLoadTime")
+    private int maxPageLoadTime;
+
+    @Property(value = "implicitWait")
+    private int implicitWait;
+
+    @Property(value = "videoFeature")
+    private String videoFeature;
+
+    @Property(value = "videoFolder")
+    private String videoFolder;
+
+    @Property(value = "appType")
+    private String appType;
+
+    @Property(value = "retryCount")
+    private String retryCount;
+
+    @Property(value = "url")
+    private String url;
+
+    public String getBrowser() {
+        return browser;
+    }
+
+    public int getMaxPageLoadTime() {
+        return maxPageLoadTime;
+    }
+
+    public int getImplicitlyWait() {
+        return implicitWait;
+    }
+
+    public String getVideoFeature() {
+        return videoFeature;
+    }
+
+    public String getVideoFolder() {
+        return videoFolder;
+    }
+
+    public String getAppType() {
+        return appType;
+    }
+
+    public int getRetryCount() {
+        return Integer.parseInt(retryCount);
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+
+}
