621bf45a188eb6779ba7f28b96f974cbde21f723
Shaik Bikarsha <87685116+shaikbikarsha@users.noreply.github.com>
2021-12-29 01:14:32 +0530

    Create ValueAssigner.java

---
+++ src/main/java/controllers/ValueAssigner.java
@@ -0,0 +1,31 @@
+package controllers;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+
+import java.awt.*;
+import java.io.File;
+
+public class ValueAssigner {
+    public static ConfigPropertyReader appConfig = new ConfigPropertyReader();
+
+    public static String browser = appConfig.getBrowser();
+    public static int maxPageLoadTime = appConfig.getMaxPageLoadTime();
+    public static int implicitWait = appConfig.getImplicitlyWait();
+    public static String videoFeature = appConfig.getVideoFeature();
+    public static String website = appConfig.getUrl();
+    public static String videoFolder = appConfig.getVideoFolder();
+    public static String appType = appConfig.getAppType();
+    public static int retryCount = appConfig.getRetryCount();
+    public static String fileSeparator = File.separator;
+    public static String osName = System.getProperty("os.name");
+    public static String osArchitecture = System.getProperty("os.arch");
+    public static String osVersion = System.getProperty("os.version");
+    public static String osBit = System.getProperty("sun.arch.data.model");
+    public static String projectWorkingDirectory = System.getProperty("user.dir");
+    public static Robot re;
+    public static Alert al;
+    public static Select se;
+    public static Actions ac;
+}
